14799d86812b281798fd1c1302733f6c88bd625d
EsaKris <esachris00@gmail.com>
2023-10-11 02:52:45 +0100

    modified:   homepage/models.py

--- homepage/models.py
+++ homepage/models.py
@@ -11,6 +11,9 @@ class Attendees(models.Model):
     Phone = models.CharField(max_length=20)
     Gender = models.CharField(max_length = 150, choices=GENDER)
     Local_Assembly = models.CharField(max_length=250)
+    Nationality = models.CharField(max_length=100)
+    State_of_Origin = models.CharField(max_length=100)
+    Local_Government_Area = models.CharField(max_length=200)
     
     
 
