610859d5199d704e5078cb59243c56cf3176b762
john okojere <59104769+john-okojere@users.noreply.github.com>
2023-10-11 02:47:29 +0100

    Model

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+GENDER = (
+    ('Male', 'Male'),
+    ('Female', 'Female')
+)
+
+class Attendees(models.Model):
+    Full_Name = models.CharField(max_length=250)
+    Email = models.EmailField()
+    Phone = models.CharField(max_length=20)
+    Gender = models.CharField(max_length = 150, choices=GENDER)
+    Local_Assembly = models.CharField(max_length=250)
+    
+    
+
+    
