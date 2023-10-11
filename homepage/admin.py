f0e72e0d19e34241f9fb09fbda24981ec1e5effe
john okojere <59104769+john-okojere@users.noreply.github.com>
2023-10-11 04:19:19 +0100

    admin filter

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,4 +1,11 @@
+from import_export.admin import ImportExportModelAdmin
+from import_export.admin import ImportExportMixin
 from django.contrib import admin
 from .models import Attendees
 
-admin.site.register(Attendees)
+
+class RegisterAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ('Full_Name','Email', 'Phone', 'Gender','Local_Assembly', 'Nationality', 'State_of_Origin', 'Local_Government_Area', 'Are_you_a_pastor', 'will_you_be_camping')
+    list_filter = ('Full_Name','Gender','Local_Assembly', 'Nationality', 'State_of_Origin', 'Local_Government_Area', 'Are_you_a_pastor', 'will_you_be_camping')
+    search_fields =  ('Full_Name','Gender','Local_Assembly', 'Nationality', 'State_of_Origin', 'Local_Government_Area', 'Are_you_a_pastor', 'will_you_be_camping')
+admin.site.register(Attendees, RegisterAdmin)
