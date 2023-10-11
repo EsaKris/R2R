4f6dcfea19c3dfbca8af5d7cfca7f1b635693334
john okojere <59104769+john-okojere@users.noreply.github.com>
2023-10-11 03:53:53 +0100

    form

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .forms import AttendeeForm
 
 def register(request):
-    return render(request, 'home/register.html')
+    if request.method == "POST":
+        form =AttendeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form =AttendeeForm()
+    return render(request, 'home/register.html', {'form':form})
