d509ba7a4728987dfbef8dede9c4ab4f084e0f49
john okojere <59104769+john-okojere@users.noreply.github.com>
2023-10-13 17:08:27 +0000

    modified:   R2R/__pycache__/__init__.cpython-310.pyc 	modified:   R2R/__pycache__/settings.cpython-310.pyc 	modified:   R2R/__pycache__/urls.cpython-310.pyc 	modified:   R2R/__pycache__/wsgi.cpython-310.pyc 	modified:   homepage/__pycache__/__init__.cpython-310.pyc 	modified:   homepage/__pycache__/admin.cpython-310.pyc 	modified:   homepage/__pycache__/apps.cpython-310.pyc 	modified:   homepage/__pycache__/forms.cpython-310.pyc 	modified:   homepage/__pycache__/models.cpython-310.pyc 	modified:   homepage/__pycache__/urls.cpython-310.pyc 	modified:   homepage/__pycache__/views.cpython-310.pyc 	modified:   homepage/migrations/__pycache__/0001_initial.cpython-310.pyc 	modified:   homepage/migrations/__pycache__/__init__.cpython-310.pyc 	modified:   homepage/static/css/style.css 	new file:   homepage/static/img/logo.png 	new file:   homepage/static/js/scripts.js 	modified:   templates/home/index.html

---
+++ homepage/static/js/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("nav-icons").addEventListener('click', function(){
+    if(document.getElementById("nav-item-list").style.display == "block"){
+        document.getElementById("nav-item-list").style.display ="none";
+    }else{
+        document.getElementById("nav-item-list").style.display ="block";
+    }
+})
