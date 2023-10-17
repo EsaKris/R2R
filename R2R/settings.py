cb4712e5206c669ea921b6709f013e132d7c9d96
John Okojere <59104769+john-okojere@users.noreply.github.com>
2023-10-17 16:13:24 +0000

    hjg

--- R2R/settings.py
+++ R2R/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 
     #packages
     'import_export',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -128,6 +130,8 @@ STATIC_ROOT = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = 'media/'
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
