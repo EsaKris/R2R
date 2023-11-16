032d4c2fe56200c7012f2a27a952f0fb026a7201
john-okojere <johnokojere08@gmail.com>
2023-11-16 16:47:19 +0100

    modified:   R2R/__pycache__/settings.cpython-311.pyc 	modified:   R2R/settings.py 	modified:   templates/home/attendee_list.html

--- R2R/settings.py
+++ R2R/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2$#4sy=d#f5a6z@!i&sb62x9ujfx9%v-!^9qlah!ou4bcj+m&n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
