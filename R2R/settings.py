02b76a8d2cb4e97f175d58ba471c0b87ab894a48
john-okojere <johnokojere08@gmail.com>
2023-11-21 15:32:04 +0100

    modified:   R2R/__pycache__/settings.cpython-311.pyc 	modified:   R2R/settings.py 	modified:   db.sqlite3 	modified:   homepage/__pycache__/models.cpython-311.pyc 	modified:   homepage/models.py 	new file:   media/QR_CODE/John_okojere_QR_CODE.png 	new file:   media/QR_CODE/John_okojerw_QR_CODE.png 	new file:   media/QR_CODE/sjikz_QR_CODE.png 	modified:   templates/base.html 	modified:   templates/home/attendee_list.html

--- R2R/settings.py
+++ R2R/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = '/static/'
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
