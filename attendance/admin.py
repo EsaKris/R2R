4c9e930939946f2b4f98addc06e77e432b380546
john-okojere <johnokojere08@gmail.com>
2023-12-16 04:24:54 +0100

    deleted:    R2R.zip 	modified:   attendance/__pycache__/admin.cpython-311.pyc 	modified:   attendance/admin.py

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CreateAttendance, Attendance
 
-# Register your models here.
+admin.site.register(CreateAttendance)
+admin.site.register(Attendance)
